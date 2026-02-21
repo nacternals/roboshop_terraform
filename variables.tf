12e5c8dc3c46dfbf1a8695d576ecbe5d5cf2da3f
devops_01 <devops_01@gmail.com>
2026-02-21 15:29:36 +0530

    Added basic VPC creation logic....

--- variables.tf
+++ variables.tf
@@ -3,5 +3,16 @@ variable "aws_region" {
 }
 
 variable "project" {
-  type = string
+  type        = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/stage/prod)"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for VPC"
 }
